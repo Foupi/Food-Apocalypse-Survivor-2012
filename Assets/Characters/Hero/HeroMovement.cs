e9cdc3e2ecd15dc8a897a708712579f486777387
Chaussurre <Samuel@Corno.com>
2020-06-13 00:40:33 +0200

    Feature : Added hero, moving up

---
+++ Assets/Characters/Hero/HeroMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    private Rigidbody2D RB;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(GetDirection());
+    }
+
+    private Vector3 GetDirection()
+    {
+        return Vector3.up; //FIXME
+    }
+
+    private void Move(Vector3 Direction)
+    {
+        RB.velocity = Direction.normalized * speed;
+    }
+}
