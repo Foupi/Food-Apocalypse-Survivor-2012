ea55091457b80eb9e6f199df661aaa1387330a92
Pierre Foucart <pierre.foucart@epita.fr>
2020-06-13 03:21:42 +0200

    feature: Allow the pellets to disappear upon touching someone
    They also disappear upon reaching a certain distance.
    
    Fighters also now shoot at a rate of 1 pellet/second.

--- Assets/Characters/Fighters/FighterBehaviour.cs
+++ Assets/Characters/Fighters/FighterBehaviour.cs
@@ -13,24 +13,44 @@ public class FighterBehaviour : MonoBehaviour
     // Speed
     public float Speed;
 
+    // Time between two shots
+    public float CoolDown;
+
+    // Time remaining before the next shot
+    private float remainingCoolDown;
+
+    // Collider of the fighter
+    private Collider2D fighterCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        remainingCoolDown = 0.0f;
+        fighterCollider = this.GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        // FIXME
-        Shoot();
+        if (remainingCoolDown <= 0.0f)
+            Shoot();
+        else
+            remainingCoolDown -= Time.fixedDeltaTime;
     }
 
-    // Shoots food
+    // Shoots food and resets the cooldown
     private void Shoot()
     {
+        ThrowFood();
+        remainingCoolDown = CoolDown;
+    }
+    
+    // Instanciates the food to be thrown, and throws it.
+    private void ThrowFood()
+    {
         GameObject p = Instantiate(food, transform.position, Quaternion.identity);
+        Collider2D projectileCollider = p.GetComponent<Collider2D>();
+        Physics2D.IgnoreCollision(projectileCollider, fighterCollider);
         Vector3 direction = target.position - p.transform.position;
         p.GetComponent<Rigidbody2D>().velocity = direction.normalized * Speed;
     }
---
+++ Assets/Characters/Fighters/FoodImpact.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodImpact : MonoBehaviour
+{
+    // Max duration a food projectile can reach before disappearing
+    public float Range;
+
+    // Point from which the food projectile was thrown
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 currentDistanceVector = currentPosition - startPosition;
+        float currentDistance = currentDistanceVector.magnitude;
+        if (currentDistance >= Range)
+            disappear();
+    }
+
+    // Reacts to contact with something
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out HeroMovement hero))
+        {
+            disappear();
+        }
+        if (other.TryGetComponent(out FighterBehaviour fighter))
+        {
+            disappear();
+        }
+
+    }
+
+    // Time to die, little piece of whatever food you are... 
+    private void disappear()
+    {
+        Destroy(gameObject);
+    }
+}
