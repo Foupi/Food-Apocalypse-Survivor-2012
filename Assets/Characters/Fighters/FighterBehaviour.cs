5d921cc0a023415864a7dd4e3bffa45d7ea6cd81
Pierre Foucart <pierre.foucart@epita.fr>
2020-06-13 02:23:56 +0200

    feature: Add a fighter which shoots white pellets
    However, he does shoot a lot

---
+++ Assets/Characters/Fighters/FighterBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterBehaviour : MonoBehaviour
+{
+    // Aiming coordinates
+    public Transform target;
+
+    // Projectile
+    public GameObject food;
+
+    // Speed
+    public float Speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // FIXME
+        Shoot();
+    }
+
+    // Shoots food
+    private void Shoot()
+    {
+        GameObject p = Instantiate(food, transform.position, Quaternion.identity);
+        Vector3 direction = target.position - p.transform.position;
+        p.GetComponent<Rigidbody2D>().velocity = direction.normalized * Speed;
+    }
+}
