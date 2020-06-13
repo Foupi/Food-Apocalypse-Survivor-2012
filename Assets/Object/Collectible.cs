5d8fdf730863247d9aac9266f9116218a00bc83e
Chaussurre <Samuel@Corno.com>
2020-06-13 02:22:10 +0200

    Feature : Donut that makes you change scene

---
+++ Assets/Object/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out HeroMovement Hero))
+        {
+            OnCollectEffect(Hero);
+        }
+    }
+
+    protected virtual void OnCollectEffect(HeroMovement Hero) { }
+}
---
+++ Assets/Object/Donut/Donut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Donut : Collectible
+{
+    public string SceneName;
+    protected override void OnCollectEffect(HeroMovement Hero)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
